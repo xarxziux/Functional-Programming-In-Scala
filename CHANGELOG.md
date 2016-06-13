9641499187f456328d426e16ba3c7d5828e54865
xarxziux <hairy.one@virgin.net>
2016-06-13 15:03:38 +0000

    Working version of pascal() function; change log added

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Function Programming in Scala Change Log
+
+## 2016-06-13
+
+### Changed
+- recfun/Main.scala: working version of Pascal() function saved.
+
+### Added
+- Change log created
--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,36 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    
+    def expandList (inc: Int, numList: List[Int]): List [Int] =
+      if (numList.tail.isEmpty)
+        List (inc + numList.head)
+      else
+        inc + numList.head :: expandList (inc + numList.head, numList.tail)
+
+    def buildBaseList (elemCount: Int): List [Int] = 
+      if (elemCount == 0)
+        List (1)
+      else
+        1 :: buildBaseList (elemCount - 1)
+        
+    def expandCount (count: Int, exList: List [Int]): List [Int] =
+      if (count == 0)
+        exList
+      else 
+        expandCount (count - 1, expandList (0, exList))
+      
+    if ((c > r) || (c < 0) || (r < 0))
+      0
+    else if ((c == r) || (c == 0))
+      1
+    else {
+      val fullList = expandCount (r - c, buildBaseList (c))
+      fullList (c)
+    }
+  }
+
   
   /**
    * Exercise 2
@@ -26,11 +55,6 @@ object Main {
    */
     def countChange(money: Int, coins: List[Int]): Int = ???
     
-    def expandList (inc: Int, numList: List[Int]): List [Int] =
-      if (numList.tail.isEmpty)
-        List (inc + numList.head)
-      else
-        inc + numList.head :: expandList (inc + numList.head, numList.tail)
     
   
   
