f3e46a3785a34f4df7dbb93c3a5eb5c55ee86cbc
xarxziux <hairy.one@virgin.net>
2016-06-30 13:35:38 +0000

    Two working versions of filterAcc() method added

--- Week3/IntSets.scala
+++ Week3/IntSets.scala
@@ -6,10 +6,14 @@ abstract class IntSet {
   def incl (x: Int): IntSet
   def contains (x: Int): Boolean
   def union (other: IntSet): IntSet
-  def filterAcc (f: Int => Boolean, acc: IntSet): IntSet
+  def filterAcc1 (f: Int => Boolean, acc: IntSet): IntSet
+  def filterAcc2 (f: Int => Boolean, acc: IntSet): IntSet
   
-  def filter (f: Int => Boolean): IntSet =
-    filterAcc (f, Empty)
+  def filter1 (f: Int => Boolean): IntSet =
+    filterAcc1 (f, Empty)
+
+  def filter2 (f: Int => Boolean): IntSet =
+    filterAcc2 (f, Empty)
 }
 
 object Empty extends IntSet {
@@ -17,7 +21,8 @@ object Empty extends IntSet {
   def incl (x: Int): IntSet = new NonEmpty (x, Empty, Empty)
   override def toString = "."
   def union (other: IntSet): IntSet = other
-  def filterAcc (f:Int => Boolean, acc: IntSet): IntSet = Empty
+  def filterAcc1 (f:Int => Boolean, acc: IntSet): IntSet = acc
+  def filterAcc2 (f:Int => Boolean, acc: IntSet): IntSet = acc
 }
 
 class NonEmpty (elem: Int, left: IntSet, right: IntSet) extends IntSet {
@@ -37,10 +42,19 @@ class NonEmpty (elem: Int, left: IntSet, right: IntSet) extends IntSet {
   def union (other: IntSet): IntSet = 
     left union right union other incl elem
     
-  def filterAcc (f: Int => Boolean, acc: IntSet): IntSet = {
-    val newAcc = right filterAcc (f, left filterAcc (f, acc))
-    if (f(elem)) newAcc incl elem
-    else newAcc
+  def filterAcc1 (f: Int => Boolean, acc: IntSet): IntSet = {
+    if (f(elem)) 
+      right filterAcc1 (f, left filterAcc1 (f, acc)) incl elem
+    else 
+      right filterAcc1 (f, left filterAcc1 (f, acc))
+  }
+  
+  def filterAcc2 (f: Int => Boolean, acc: IntSet): IntSet = {
+    val lAcc = left filterAcc2 (f, acc)
+    val rAcc = right filterAcc2 (f, lAcc)
+
+    if (f (elem)) rAcc incl elem
+    else rAcc
   }
 }
 
